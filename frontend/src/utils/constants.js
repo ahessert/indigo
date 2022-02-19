23f55d4f67111f425aecc03428485cefb6d5c1c8
Adrian Sun <adriansun2@gmail.com>
2022-02-19 00:18:54 -0800

    allow site to use ropsten

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,9 @@
-export const CHAIN_ID = 1313161555;
-export const CHAIN_ID_0x = '0x4e454153';
+// AURORA
+// export const CHAIN_ID = 1313161555;
+// export const CHAIN_ID_0x = '0x4e454153';
+// ROPSTEN
+export const CHAIN_ID = 3;
+export const CHAIN_ID_0x = '1';
 
 export const blockExplorerUrl = 'https://explorer.mainnet.aurora.dev/';
 export const developerDocUrl =
